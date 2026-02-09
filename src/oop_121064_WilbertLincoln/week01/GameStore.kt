0a105605426d3b38f0d9c9c252a12a513fcf5c2f
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-02-09 21:36:41 +0700

    week01: (tugas 2) implement recipt printing

--- src/oop_121064_WilbertLincoln/week01/GameStore.kt
+++ src/oop_121064_WilbertLincoln/week01/GameStore.kt
@@ -4,6 +4,14 @@ fun main() {
     val gameTitle = "GTA VI"
     val price = 800000
 
+    val finalPrice = calculateDiscount(price)
+    printRecipt(title = gameTitle, finalPrice = finalPrice)
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.8).toInt() else (price * 0.9).toInt()
+
+fun printRecipt(title: String, finalPrice: Int) {
+    println("--- Struk Pembelian ---")
+    println("Game: $title")
+    println("Harga Akhir: Rp $finalPrice")
+}
