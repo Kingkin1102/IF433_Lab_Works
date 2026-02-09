54d3e9af109621456a91a689f25032b67a4a1995
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-02-09 21:31:10 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_121064_WilbertLincoln/week01/GameStore.kt
+++ src/oop_121064_WilbertLincoln/week01/GameStore.kt
@@ -3,4 +3,7 @@ package oop_121064_WilbertLincoln.week01
 fun main() {
     val gameTitle = "GTA VI"
     val price = 800000
+
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.8).toInt() else (price * 0.9).toInt()
