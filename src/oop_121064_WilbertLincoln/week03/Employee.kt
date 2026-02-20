065599a30c0e6acc9b6840fa4a587d87fa5d0df3
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-02-20 11:07:52 +0700

    week03: simulate stack overflow error in setter

--- src/oop_121064_WilbertLincoln/week03/Employee.kt
+++ src/oop_121064_WilbertLincoln/week03/Employee.kt
@@ -2,4 +2,9 @@ package oop_121064_WilbertLincoln.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            //CODE RECURSIVE!!!
+            this.salary = value
+        }
 }
---
+++ src/oop_121064_WilbertLincoln/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_121064_WilbertLincoln.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
