95362b8360c157f341b087865fdbf1d55c03214d
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-02-20 11:37:30 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_121064_WilbertLincoln/week03/Weapon.kt
@@ -0,0 +1,23 @@
+package oop_121064_WilbertLincoln.week03
+
+class Weapon(val name: String) {
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Peringatan: Damage tidak boleh negatif! Nilai damage saat ini tetap $field.")
+            } else if (value > 1000) {
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() {
+            return when {
+                damage > 800 -> "Legendary"
+                damage > 500 -> "Epic"
+                else -> "Common"
+            }
+        }
+}
