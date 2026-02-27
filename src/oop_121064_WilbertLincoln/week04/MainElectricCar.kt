f82169f1a69f025ae7f56b24685cba4d2221437a
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-02-27 12:48:05 +0700

    week04: (task 1) test ElectricCar in main

---
+++ src/oop_121064_WilbertLincoln/week04/MainElectricCar.kt
@@ -0,0 +1,9 @@
+package oop_121064_WilbertLincoln.week04
+
+fun main() {
+    val myElectricCar = ElectricCar("Tesla", 4, 85)
+
+    myElectricCar.accelerate()
+    myElectricCar.honk()
+    myElectricCar.openTrunk()
+}
