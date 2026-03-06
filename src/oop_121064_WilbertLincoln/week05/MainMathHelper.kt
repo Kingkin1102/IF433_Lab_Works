5c91af528f9487cde1c5d15e49057ce6ea7924f8
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-03-06 13:33:32 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_121064_WilbertLincoln/week05/MainMathHelper.kt
@@ -0,0 +1,9 @@
+package oop_121064_WilbertLincoln.week05
+
+fun main() {
+    val mathHelper = MathHelper()
+
+    println("Luas Persegi (5): ${mathHelper.hitungLuas(5)}")
+    println("Luas Persegi Panjang (P = 5, L = 10): ${mathHelper.hitungLuas(5,10)}")
+    println("Luas Lingkaran (7.0): ${mathHelper.hitungLuas(7.0)}")
+}
