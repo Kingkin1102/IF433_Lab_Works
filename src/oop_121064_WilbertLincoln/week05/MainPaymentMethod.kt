721cb5116962a9ce3f34ad5bb3e0d88f0a267cbf
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-03-06 14:17:04 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_121064_WilbertLincoln/week05/MainPaymentMethod.kt
@@ -0,0 +1,15 @@
+package oop_121064_WilbertLincoln.week05
+
+fun main() {
+    val eWallet = EWallet("Gopay Erpe",50000.0)
+    val creditCard = CreditCard("CC BCA Erpe", 100000.0)
+
+    val paymentList: List<PaymentMethod> = listOf(eWallet, creditCard)
+    val transactionAmount = 75000.0
+
+    for (method in paymentList) {
+        println("-> Memproses pembayaran untuk: ${method.accountName}")
+
+        method.processPayment(transactionAmount)
+    }
+}
