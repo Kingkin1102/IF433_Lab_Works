9399d5c55f423fe004e6bb14f2a6861c26126d9e
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-03-13 15:19:16 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_121064_WilbertLincoln/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_121064_WilbertLincoln.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
