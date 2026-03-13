add131bb2a8ec3d5ad98382848537896ee096550
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-03-13 15:29:35 +0700

    week06: (task) test smart home system logic in main

--- src/oop_121064_WilbertLincoln/week06/TugasMain.kt
+++ src/oop_121064_WilbertLincoln/week06/TugasMain.kt
@@ -5,4 +5,13 @@ fun main() {
     val lamp = SmartLamp("L01", "Ruang Tamu")
     val speaker = SmartSpeaker("S01", "Google Nest Dapur")
     val cctv = SmartCCTV("C01", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+    println("---")
+    hub.turnOffAllSwitches()
 }
