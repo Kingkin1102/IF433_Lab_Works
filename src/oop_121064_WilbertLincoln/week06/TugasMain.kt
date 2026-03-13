434e2f056c27d506a6554ba598568c419e14c5fd
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-03-13 15:28:12 +0700

    week06: (task) instantiate smart device in main

---
+++ src/oop_121064_WilbertLincoln/week06/TugasMain.kt
@@ -0,0 +1,8 @@
+package oop_121064_WilbertLincoln.week06
+
+fun main() {
+    println("\n=== TESTING SMART HOME SYSTEM ===")
+    val lamp = SmartLamp("L01", "Ruang Tamu")
+    val speaker = SmartSpeaker("S01", "Google Nest Dapur")
+    val cctv = SmartCCTV("C01", "Ezviz Garasi")
+}
