1bf611ff9ac182cc70511ace713a7d5cc879fbc0
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-04-17 15:01:07 +0700

    week08: create nested classes for Order

---
+++ src/oop_121064_WilbertLincoln/week08/OrderModels.kt
@@ -0,0 +1,6 @@
+package oop_121064_WilbertLincoln.week08
+
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
