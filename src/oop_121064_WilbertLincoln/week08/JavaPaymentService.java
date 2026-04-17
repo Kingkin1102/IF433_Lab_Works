9735dabbac4c255defe3fcd629dfe61862301176
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-04-17 15:54:23 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_121064_WilbertLincoln/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_121064_WilbertLincoln.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
