9b78916ae7ca0a48ba743f5cfcd822d9f32e3108
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2026-05-06 23:38:35 +0700

    week10: (task) populate coin repository

--- src/oop_121064_WilbertLincoln/week10/CryptoDashboard.kt
+++ src/oop_121064_WilbertLincoln/week10/CryptoDashboard.kt
@@ -4,4 +4,8 @@ fun main() {
     println("=== INISIALISASI DASHBOARD CRYPTO ===")
 
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.25))
+    coinRepo.add(Coin("ETH", 15.5))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
